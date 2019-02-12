ecf36bdd845690827e90e5c0a68f536a7e06d811
Dmitry Titarenko <dmitry.titarenko@graph.uk>
2019-02-12 17:17:33 +0500

    ST-10: setup address as rich text

--- MapBlock.Controller.js
+++ MapBlock.Controller.js
@@ -1,13 +1,31 @@
 angular.module("umbraco").controller("MapBlock.Controller", function ($scope) {
 
-    function Item() {
+	function Item() {
 		this.title = '';
 		this.address = '';
 		this.ctaText = '';
-        this.ctaLink = '';
-        this.latitude = '';
-        this.longitude = '';
-    }
+		this.ctaLink = '';
+		this.latitude = '';
+		this.longitude = '';
+	}
 
-    $scope.control.value = $scope.control.value || new Item();
+	$scope.control.value = $scope.control.value || new Item();
+
+	$scope.addressR
+		label: 'bodyText',
+		description: 'Load some stuff here',
+		view: 'rte',
+		value: $scope.control.value.address,
+		config: {
+			editor: {
+				toolbar: ["code", "undo", "redo", "cut", "styleselect", "bold", "italic", "alignleft", "aligncenter", "alignright", "bullist", "numlist", "link"],
+				stylesheets: ["rte"],
+				dimensions: { height: 400 },
+				valueType:"STRING"
+			}
+		}
+	},
+	$scope.$watch("addressRte.value", function (newValue, oldValue) {
+		$scope.control.value.address = newValue;
+	});
 });
